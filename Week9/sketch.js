01b739f2d4530a76bdd283742b2fe76c1fe811b0
tfladager <170553495+tfladager@users.noreply.github.com>
2025-04-06 17:42:54 -0600

    maybenow

--- Week9/sketch.js
+++ Week9/sketch.js
@@ -12,7 +12,7 @@ function draw()
     // head
     fill(255, 255, 255);
     circle(250,200,175);
-    triangle(220,75,250,5,280,75)
+    triangle(220,175,250,5,280,175)
    
     // eyes
     strokeWeight(20);
