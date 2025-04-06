c6ff5e5737259fe68ad528c1393e5ab309f44733
tfladager <170553495+tfladager@users.noreply.github.com>
2025-04-06 16:12:12 -0600

    fixchange

--- Week9/sketch.js
+++ Week9/sketch.js
@@ -1,12 +1,10 @@
-function setup (){
-    createCanvas (400, 400);
+function setup ()
+{
+    createCanvas (400,400);
 }
 
-function draw () {
-    if (mouseIsPressed) {
-        fill (0);
-    } else {
-        fill (255);
-    }
-    elippse (mouseX, mouseY, 80, 80);
+function draw () 
+{
+    background(220);
+    console.log("hi");
 }
