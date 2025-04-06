c8ff257619329d8dcf452568e00560910f840f24
tfladager <170553495+tfladager@users.noreply.github.com>
2025-04-06 16:51:49 -0600

    self portrait

--- Week9/sketch.js
+++ Week9/sketch.js
@@ -1,8 +1,53 @@
-function setup ()
+function setup()
 {
-    createCanvas (300,500);
+    createCanvas(500, 600);
 }
-function draw () 
+
+function draw()
 {
-    background(220);
+    background(120,45,78);
+    textSize(22)
+    text("I'm trying!", 10,80);
+
+    // head
+    fill(255, 204, 0);
+    circle(250,100,175);
+   
+    // eyes
+    strokeWeight(10);
+    fill(0);
+    point(200,75);
+    point(285,75);
+
+    // nose
+    point(245,90);
+    
+    // mouth
+    ellipse(245, 135, 30, 45)
+
+    // hair
+    line(130,175,175,50);
+    line(325,50,360,175);
+    // body
+    fill(10, 24, 120);
+    rect(200,185,100,150);
+    
+    // decoration
+    fill(255);
+    triangle(220,320,250,220,280,320)
+    // right arm
+    fill(10, 24, 120);
+    rect(300,195,50,10);
+    // left arm
+    rect(150,195,50,10);
+    // left leg
+    rect(200,335,10,50);
+    // right leg
+    rect(290,335,10,50);
+    
+    fill(140);
+    textSize(30);
+    text("Tammi Fladager",270,500 );
+
+
 }
