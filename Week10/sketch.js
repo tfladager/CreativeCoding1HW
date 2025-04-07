b0884a740c7839005cbf6a98d18e7ddbbbc75a80
tfladager <170553495+tfladager@users.noreply.github.com>
2025-04-06 20:26:51 -0600

    tri times

--- Week10/sketch.js
+++ Week10/sketch.js
@@ -57,15 +57,15 @@ function draw()
     triangle3X += TriangleDirection3;
     if (triangleX >=150 || triangleX<=300)
     {
-        TriangleDirection1 += -1;
+        TriangleDirection1 *= -1;
     }
     if (triangle2X >=150|| triangle2X<=400)
         {
-            TriangleDirection2 += -1;
+            TriangleDirection2 *= -1;
         }
         if (triangle3X >=150 || triangle3X<=400)
             {
-                TriangleDirection3 += -1;
+                TriangleDirection3 *= -1;
             }    
 
    
